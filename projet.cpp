11c929cd6d4bee78cca59fd317e705b16fc1e2cb
zmh <871329248@qq.com>
2022-04-05 03:07:57 +0800

    class

--- projet.cpp
+++ projet.cpp
@@ -1,5 +1,8 @@
 #include <cstdlib>
 #include <iostream>
+#include <ostream>
+#include <vector>
+#include <list>
 #include <fstream>
 #include <cmath>
 #include <time.h>
@@ -43,7 +46,6 @@ return os ;
 }
 
 
-
 vector<double> Subdiv(double a,int N)
 {
     vector<double> xi = vector<double>(N+1);
@@ -64,20 +66,101 @@ void print_v(vector<double> A)    //affichage de vecteur
 }
 
 
-int numgb(int N,int M,int i,int j)
+int numgb(int N, const Point &P)
 {
-    return (N+1)*j+i;
+    return (N+1)*P.y+P.x;
+}
+
+
+Point Point::invnumgb(int N, int s){
+    x = s%(N+1);// i: le reste de la division s par (N+1)
+    y = s/(N+1);// j: le quotient de la division s par (N+1)
+    return *this;  
+}
+
+
+int numint(int N, const Point &P)
+{
+    return (P.y-1)*(N-1)+P.x-1; 
+}
+
+Point Point::invnumint(int N, int k){
+    x = k%(N+1)+1; // i-1 est le reste de la division k par (N+1)
+    y = k/(N+1)+1; // j-1 est le quotient de la division k par (N+1)
+    return *this;
+}
+
+int num_int_gb(int N, int k){
+    Point P;
+    P = P.invnumint(N,k);
+    int s=numgb(N,P);
+    return s;
+}
+
+int num_gb_int(int N, int s){
+    Point P;
+    P = P.invnumgb(N,s);
+    int k= numint(N,P);
+    return k;
+}
+
+
+
+ostream & operator <<(ostream &os, const Triangle &T){
+    os<<"("<<T.a<<","<<T.b<<","<<T.c<<")" ;
+    return os ;
+}
+
+
+vector<Triangle>  maillageTR(int N,int M)
+{
+    vector<Triangle> TRG = vector<Triangle>(2*N*M);
+    Point P;
+    int i=0;
+    for(int s=0;s<(N+1)*(M)-1;s++){
+        P.invnumgb(N,s);
+        if (P.x == N){
+            continue;
+        }
+        else{
+            TRG[2*i].a = s;
+            TRG[2*i].b = s+N+1;
+            TRG[2*i].c = s+N+2;
+            TRG[2*i+1].a = s;
+            TRG[2*i+1].b = s+1;
+            TRG[2*i+1].c = s+N+2;
+            i++;
+        }
+    }
+    return TRG;
 }
 
 
 
 
 
-
 int main(){
-    vector<double> xi = Subdiv(5,20);
-    print_v(xi);
-     
+
+    Point P ;
+    P.invnumgb(6,0);
+    
+    vector<Triangle> TRG =  maillageTR(4,2);
+    for (int i=0;i<16;i++){
+    cout<<TRG[i]<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
 }
 
 
--- projet.hpp
+++ projet.hpp
@@ -16,28 +16,44 @@ public:
     double x;
     double y;
     Point(double x0=0,double y0 =0):x(x0),y(y0){}
-    Point& operator+=( const Point & P) {x+=P.x ; y+=P.y ; return *this ; }
-    Point& operator-=( const Point & P) {x-=P.x ; y-=P.y ; return *this ; }
-    Point& operator *= (double a ){ x*=a ; y*=a ; return *this ; }
-    Point& operator/= (double a ){ x/=a ; y/=a ; return *this ; }
+    Point& operator+=( const Point & P) {x+=P.x ; y+=P.y ; return *this;}
+    Point& operator-=( const Point & P) {x-=P.x ; y-=P.y ; return *this;}
+    Point& operator *= (double a ){ x*=a ; y*=a ; return *this;}
+    Point& operator/= (double a ){ x/=a ; y/=a ; return *this;}
+    Point invnumgb(int N, int s);
+    Point invnumint(int N, int k);
 };
 Point operator + ( const Point& , const Point &);
 Point operator - ( const Point& , const Point &);
-Point operator * ( const Point& ,double a ) ;
-Point operator * (double a , const Point & ) ;
-Point operator / ( const Point& ,double a ) ;
+Point operator * ( const Point& ,double a);
+Point operator * (double a , const Point &);
+Point operator / ( const Point& ,double a);
 //bool operator == ( const P oin t& , const P oin t& ) ;
 //bool operator != ( const P oin t& , const P oin t& ) ;
 ostream & operator <<(ostream &, const Point &);
+int numgb(int N, const Point &);
+int numint(int N, const Point&);
+int num_int_gb(int N, int k);
+int num_gb_int(int N, int s);
 
 
 vector<double> Subdiv(double a,int N);
 void print_v(vector<double> A);
-int numgb(int N,int M, const Point &P);
 
 
 
+class Triangle
+{
+    private:
 
+    public:
+    double a;
+    double b;
+    double c;
+    Triangle(double a0 = 0,double b0 = 0,double c0 = 0):a(a0),b(b0),c(c0){} 
+};
+ostream & operator <<(ostream &, const Triangle &);
+vector<Triangle>  maillageTR(int N,int M);
 
 
 
